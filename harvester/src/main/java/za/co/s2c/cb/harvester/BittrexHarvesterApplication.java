c510fbaf834297c9d6fc7ce09ee253cdd80f2da0
Jan-Rudolph Buhrmann <jrb@s2c.co.za>
2022-04-21 15:53:55 +0200

    Configuration of hazelcast for kubernetes

--- harvester/src/main/java/za/co/s2c/cb/harvester/BittrexHarvesterApplication.java
+++ harvester/src/main/java/za/co/s2c/cb/harvester/BittrexHarvesterApplication.java
@@ -1,8 +1,11 @@
 package za.co.s2c.cb.harvester;
 
+import com.hazelcast.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import za.co.s2c.cb.bittrex_client.Streamer;
 
 @SpringBootApplication
@@ -19,5 +22,17 @@ public class BittrexHarvesterApplication {
 		new Streamer().start(new String[] {"candle_ETH-BTC_MINUTE_1"});
 		new Streamer().start(new String[] {"candle_ADA-BTC_MINUTE_1"});
 
+	}
+
+	@ConditionalOnProperty(name="is_local", havingValue = "false")
+	@Bean
+	Config config() {
+		Config config = new Config();
+		config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(false);
+		config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+		config.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true)
+				.setProperty("namespace", "default")
+				.setProperty("service-name", "hazelcast-service");
+		return config;
 	}
 }
